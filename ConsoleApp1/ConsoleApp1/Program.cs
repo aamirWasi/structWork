cd6254e61d8120aec79446d75b8e285514eca50d
aamir <wasi3aamir@gmail.com>
2019-01-27 15:56:20 +0600

    struct

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle(23,4);
+            rect1.Area();
+        }
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+            public Rectangle(double l,double w)
+            {
+                length = l;
+                width = w;
+            }
+            public void Area()
+            {
+                double sum =  length * width;
+                Console.WriteLine("Area of Rectangle is {0}",sum);
+            }
+        }
+    }
+}
